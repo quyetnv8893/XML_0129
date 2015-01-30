95e70e373b5a8a353339641839027349c1a79b4c
ThangPQ <thangpq@outlook.com.vn>
2015-01-30 15:45:03 +0700

    Added Repository class for Player, Achievement, PlayerAchievement

---
+++ XML_0129_1008_QuyetNV/Models/PlayerAchievementRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace PlayerManagement.Models
+{
+    public class PlayerAchievementRepository : IPlayerAchievementRepository
+    {
+        public List<PlayerAchievement> allPlayerAchievements;
+        private XDocument playerAchievementData;
+
+        public PlayerAchievementRepository()
+        {
+            allPlayerAchievements = new List<PlayerAchievement>();
+
+            playerAchievementData = XDocument.Load(HttpContext.Current.Server.MapPath("~/App_Data/player_management.xml"));
+            var playerAchievements = from playerAchievement in playerAchievementData.Descendants("player_achievement")
+                                     select new PlayerAchievement((int)playerAchievement.Element("number"), playerAchievement.Element("playerId").Value,
+                                         playerAchievement.Element("achievementName").Value);
+
+            allPlayerAchievements.AddRange(playerAchievements.ToList<PlayerAchievement>());
+
+        }
+
+        public IEnumerable<PlayerAchievement> GetPlayerAchievementsByPlayerID(String playerId)
+        {
+            return allPlayerAchievements.FindAll(item => item.playerId.Equals(playerId));
+        }
+
+        public PlayerAchievement GetPlayerAchievementByAchievementName(String playerId, String achievementName)
+        {
+
+            return allPlayerAchievements.Find(item => (item.achievementName.Equals(achievementName)) &&
+                                                        (item.achievementName.Equals(playerId)));
+        }
+
+        public void InsertPlayerAchievement(PlayerAchievement playerAchievement)
+        {
+
+
+            playerAchievementData.Descendants("player_achievement").FirstOrDefault().Add(new XElement("player_achievement",
+                new XElement("number", playerAchievement.number), new XElement("playerID"), playerAchievement.playerId),
+                new XElement("achievementName"), playerAchievement.achievementName);
+
+            playerAchievementData.Save(HttpContext.Current.Server.MapPath("~/App_Data/player_management.xml"));
+        }
+
+
+        public void DeletePlayerAchievement(String playerID, String achievementName)
+        {
+            playerAchievementData.Descendants("players_achievements").Elements("player_achievement").
+                Where(i => i.Element("playerID").Value.Equals(playerID)).
+                Where(i => i.Element("achievementName").Value.Equals(achievementName)).Remove();
+
+            playerAchievementData.Save(HttpContext.Current.Server.MapPath("~/App_Data/player_management.xml"));
+
+        }
+
+        public void EditPlayerAchievement(PlayerAchievement playerAchievement)
+        {
+            XElement node = playerAchievementData.Descendants("players_achievements").Elements("player_achievement")
+                .Where(i => i.Element("playerID").Value.Equals(playerAchievement.playerId))
+                .Where(i => i.Element("achievementName").Value.Equals(playerAchievement.achievementName))
+                .FirstOrDefault();
+
+            node.SetElementValue("number", playerAchievement.number);
+            node.SetElementValue("playerID", playerAchievement.playerId);
+            node.SetElementValue("achievementName", playerAchievement.achievementName);
+
+            playerAchievementData.Save(HttpContext.Current.Server.MapPath("~/App_Data/player_management.xml"));
+        }
+    }
+}
---
+++ XML_0129_1008_QuyetNV/Models/PlayerRepository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace PlayerManagement.Models
+{
+    public class PlayerRepository :IPlayerRepository
+    {
+        private List<Player> allPlayers;
+        private XDocument playerData;
+
+        public PlayerRepository()
+        {
+            allPlayers = new List<Player>();
+
+            playerData = XDocument.Load(HttpContext.Current.Server.MapPath("~/App_Data/player_management.xml"));
+            var players = from player in playerData.Descendants("player")
+                          select new Player(player.Element("clubName").Value, player.Element("id").Value, (int)player.Element("number"),
+                              player.Element("name").Value, player.Element("position").Value, (DateTime)player.Element("dateOfBirth"), player.Element("placeOfBirth").Value,
+                              (double)player.Element("weight"), (double)player.Element("height"), player.Element("description").Value, player.Element("imageLink").Value, (Boolean)player.Element("status"));
+
+            allPlayers.AddRange(players.ToList<Player>());
+
+        }
+
+
+        //Return list of players
+        public IEnumerable<Player> GetPlayers()
+        {
+            return allPlayers;
+        }
+
+        public Player GetPlayerByID(String id)
+        {
+            return allPlayers.Find(item => item.id.Equals(id));
+        }
+
+        //Add new player
+
+        public void InsertPlayer(Player player)
+        {
+            player.id = (from p in playerData.Descendants("player") orderby p.Element("id").Value descending select p.Element("id").Value).FirstOrDefault();
+
+            //Change later
+            playerData.Descendants("players").FirstOrDefault().Add(new XElement("player", new XElement("clubName", player.clubName), new XElement("id", player.id),
+                new XElement("number", player.number), new XElement("name", player.name), new XElement("position", player.position),
+                new XElement("dateOfBirth", player.dateOfBirth.Date), new XElement("placeOfBirth", player.placeOfBirth),
+                new XElement("weight", player.weight), new XElement("height", player.height), new XElement("description", player.description),
+                new XElement("imageLink", player.imageLink), new XElement("status", player.status)));
+
+            playerData.Save(HttpContext.Current.Server.MapPath("~/App_Data/player_management.xml"));
+        }
+
+        // Delete Record
+        public void DeletePlayer(String id)
+        {
+            playerData.Descendants("players").Elements("player").Where(i => i.Element("id").Value.Equals(id)).Remove();
+
+            playerData.Save(HttpContext.Current.Server.MapPath("~/App_Data/player_management.xml"));
+        }
+
+        // Edit Record
+        public void EditPlayer(Player player)
+        {
+
+            XElement node = playerData.Descendants("players").Elements("player").Where(i => i.Element("id").Value.Equals(player.id)).FirstOrDefault();
+
+            node.SetElementValue("clubName", player.clubName);
+            node.SetElementValue("id", player.id);
+            node.SetElementValue("number", player.number);
+            node.SetElementValue("position", player.position);
+            node.SetElementValue("dateOfBirth", player.dateOfBirth.Date);
+            node.SetElementValue("placeOfBirth", player.placeOfBirth);
+            node.SetElementValue("weight", player.weight);
+            node.SetElementValue("height", player.height);
+            node.SetElementValue("description", player.description);
+            node.SetElementValue("imageLink", player.imageLink);
+            node.SetElementValue("status", player.status);
+
+            playerData.Save(HttpContext.Current.Server.MapPath("~/App_Data/player_management.xml"));
+        }
+    }
+}
