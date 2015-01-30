42656becf6e224a483bd4bf847d21786e427a680
ThangPQ <thangpq@outlook.com.vn>
2015-01-30 09:21:02 +0700

    Added Achievement model classes

---
+++ XML_0129_1008_QuyetNV/Models/IAchievementRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerManagement.Models
+{
+    public interface IAchievementRepository
+    {
+        IEnumerable<Achievement> GetAchievements();
+        Achievement GetAchievementByName(String name);
+        void InsertAchievement(Achievement achievement);
+        void DeleteAchievement(String name);
+        void EditAchievement(Achievement achievement);
+    }
+}
