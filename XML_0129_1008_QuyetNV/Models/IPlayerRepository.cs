7838a09ef9d0cdbc5f20c6488eb2defef1ee87f2
ThangPQ <thangpq@outlook.com.vn>
2015-01-30 15:41:16 +0700

    Added IRepository for Player, Achievement and PlayerAchievement

---
+++ XML_0129_1008_QuyetNV/Models/IPlayerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerManagement.Models
+{
+    public interface IPlayerRepository
+    {
+        IEnumerable<Player> GetPlayers();
+        Player GetPlayerByID(String id);
+        void InsertPlayer(Player player);
+        void DeletePlayer(String id);
+        void EditPlayer(Player player);
+    }
+}
