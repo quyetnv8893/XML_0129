4717471bd9e4bfc5c325d8915e7564e20ec50f40
quyetnvse02456 <quyetnvse02456@gmail.com>
2015-01-31 11:02:31 +0700

    Reslove Conflict

---
+++ XML_0129_1008_QuyetNV/Controllers/AchievementsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PlayerManagement.Models;
+
+namespace AchievementManagement.Controllers
+{
+    public class AchievementsController : Controller
+    {
+        private IAchievementRepository _repository;
+
+        public AchievementsController()
+            : this(new AchievementRepository())
+        {
+        }
+
+        public AchievementsController(IAchievementRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        // GET: Achievements
+        public ActionResult Index()
+        {
+            return View(_repository.GetAchievements());
+        }
+
+        // GET: Achievements/Details/5
+        public ActionResult Details(string name)
+        {
+            if (name == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Achievement achievement = _repository.GetAchievementByName(name);
+            if (achievement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(achievement);
+        }
+
+        // GET: Achievements/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Achievements/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,clubName,number,name,position,dateOfBirth,placeOfBirth,weight,height,description,imageLink,status")] Achievement achievement)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.InsertAchievement(achievement);
+                return RedirectToAction("Index");
+            }
+
+            return View(achievement);
+        }
+
+        // GET: Achievements/Edit/5
+        public ActionResult Edit(string name)
+        {
+            if (name == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Achievement achievement = _repository.GetAchievementByName(name);
+            if (achievement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(achievement);
+        }
+
+        // POST: Achievements/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "name, imageLink")] Achievement achievement)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.EditAchievement(achievement);
+                //db.Entry(achievement).State = EntityState.Modified;
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(achievement);
+        }
+
+        // GET: Achievements/Delete/5
+        public ActionResult Delete(string name)
+        {
+            if (name == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Achievement achievement = _repository.GetAchievementByName(name);
+            if (achievement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(achievement);
+        }
+
+        // POST: Achievements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            _repository.DeleteAchievement(id);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ XML_0129_1008_QuyetNV/Controllers/PlayersController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PlayerManagement.Models;
+
+namespace PlayerManagement.Controllers
+{
+    public class PlayersController : Controller
+    {
+        //private ApplicationDbContext db = new ApplicationDbContext();
+        private IPlayerRepository _repository;
+
+        public PlayersController()
+            : this(new PlayerRepository())
+        {
+        }
+
+        public PlayersController(IPlayerRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        // GET: Players
+        public ActionResult Index()
+        {
+            return View(_repository.GetPlayers());
+        }
+
+        // GET: Players/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = _repository.GetPlayerByID(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // GET: Players/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Players/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,clubName,number,name,position,dateOfBirth,placeOfBirth,weight,height,description,imageLink,status")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.InsertPlayer(player);
+                return RedirectToAction("Index");
+            }
+
+            return View(player);
+        }
+
+        // GET: Players/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = _repository.GetPlayerByID(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,clubName,number,name,position,dateOfBirth,placeOfBirth,weight,height,description,imageLink,status")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.EditPlayer(player);
+                //db.Entry(player).State = EntityState.Modified;
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(player);
+        }
+
+        // GET: Players/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = _repository.GetPlayerByID(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            _repository.DeletePlayer(id);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
